292dfff2f9adcf8a98f338ca55dd66e50aee14bb
champion203 <thossaporn894@gmail.com>
2023-06-29 10:44:36 +0700

    g

--- examples/ConnectDatabase.php
+++ examples/ConnectDatabase.php
@@ -1,6 +1,6 @@
 <?php
     	$serverName = "localhost"; //172.30.208.1
-        $userName = "champ894";
+        $userName = "champ894@tu.ac.th";
         $userPassword = "1139700012961";
         $dbName = "ReserveRoom";
     
